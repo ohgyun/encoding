4674f1639ead03539257d97516bc7db326790284
Ohgyun Ahn <ohgyun@gmail.com>
2018-04-18 16:23:28 +0900

    예제 추가

--- converting.js
+++ converting.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const iconv = require('iconv-lite');
 
-// const buf = fs.readFileSync('./resources/ut.txt');
+const buf = fs.readFileSync('./resources/euckr.txt');
 
-// console.log(buf.byteLength);
-// console.log('ASCII:', buf.toString('ascii'));
-// console.log('HEX:', buf.toString('hex'));
-// console.log('UTF8:', buf.toString('utf8'));
+const utf8 = buf.toString();
+console.log('UTF8:', utf8);
 
+const euckr = iconv.decode(buf, 'euckr');
+console.log('EUCKR:', euckr);
 
+// 쓸 때
+console.log('----------------------------');
 
-// const buf = Buffer.from('가나다 뛣');
-// console.log(buf);
+const data = iconv.encode('가나다', 'euckr');
+console.log(data);
 
-const converted = iconv.encode('가나다', 'euckr');
-console.log(converted);
-
-fs.writeFileSync('./resources/euckr.txt', converted, {
+fs.writeFileSync('_euckr.txt', data, {
     encoding: null
 });
+
+console.log(iconv.decode(fs.readFileSync('_euckr.txt'), 'euckr'));
